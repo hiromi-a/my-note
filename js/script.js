b23f9cb55c2c7f74d0dc6363e082c5ce51eac335
TABLET-7G5RP80C\still <lumivalo74@gmail.com>
2023-03-06 10:11:38 +0900

    ページトップ設置

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(function () {
+  
+  // PAGE TOP
+  var pagetop = $('.js-pagetop');
+  pagetop.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+
+  // スムーススクロール
+  $('.js-anchor[href^="#"]').click(function(){
+    var time = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" ? 'html' : href);
+    var distance = target.offset().top;
+    $("html, body").animate({scrollTop:distance}, time, "swing");
+    return false;
+  });
+
+});
